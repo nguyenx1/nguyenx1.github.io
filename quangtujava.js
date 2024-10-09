ba07ad4668bda02ee3aaadfa4514115f332d0453
quangtu <boyquangtulove1@gmail.com>
2024-10-09 17:36:26 +0700

    d

--- quangtujava.js
+++ quangtujava.js
@@ -13,18 +13,47 @@ function getWeather(lat, lon, nhietdoId, rainId, buiID, locationName) {
         const bui = databui.list[0].main.aqi;
 
         // Cập nhật nội dung nhiệt độ với màu đỏ cho "°C" và giá trị nhiệt độ
-        document.querySelector(nhietdoId).innerHTML = `Nhiệt độ bây giờ ở ${locationName} là <span class="temp-value">${nhietdo}°C</span>, ${rain}`;
+        document.querySelector(nhietdoId).innerHTML = `Nhiệt độ bây giờ ở <span class="temp-value123">${locationName}</span> là <span class="temp-value">${nhietdo}°C</span>, ${rain}`;
         document.querySelector(buiID).innerHTML = `Chất lượng không khí ở ${locationName} đang ở <span class="temp-value">mức ${bui}</span>`;
     })
     .catch(function(error) {
         document.querySelector(nhietdoId).innerHTML = `Có lỗi xảy ra: ${error}`;
     });
 }
+    function currency(){
+        fetch('https://api.currencyfreaks.com/latest?apikey=c0ffb7cf1a7140adb97087cf7c4bccb3')
+        .then(function(travechotao){
+            return travechotao.json()
+        })
+        .then(function(truycap){
+            tigia = truycap.rates.VND       
+            document.querySelector('#tigia').innerHTML = `1 USD bằng ${tigia} VND`;
+            document.querySelector('form').onsubmit = function() {
+            tiengi = document.querySelector('#currency').value.toUpperCase()
+            quydoitigia = truycap.rates[tiengi];
+            if (quydoitigia !== undefined)
+            {
+                quyravnd = (tigia/quydoitigia).toFixed(2);
+                document.querySelector('#result').innerHTML = `1 ${tiengi} = ${quyravnd} VND`;
+            }
+            else{
+                document.querySelector('#result').innerHTML = `Làm chóa có tiền tệ nào tên như zậy`
+            }
+            return false;
+        }
+
+        })
+        .catch(function(biloiroi){
+            document.querySelector('#tigia').innerHTML = `Tháng sau dùng tiếp nhé`
+        })
+    }
 
 document.addEventListener('DOMContentLoaded', () => {
+    currency();
     // Lấy thời tiết ở trọ
     getWeather(21.083722, 105.772076, '#nhietdoHN', '#rainHN','#buiHN', 'trọ');
     
     // Lấy thời tiết ở quê
     getWeather(21.586592, 105.818792, '#nhietdoTN', '#rainTN','#buiTN', 'quê');
+    
 });
